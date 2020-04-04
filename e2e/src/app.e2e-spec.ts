821c10963c6c918943fb8cdff9b21a4c5ebf1134
Darshan S R <darshansr.niit@gmail.com>
2020-04-04 18:02:13 +0200

    intial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('bom-app app is running!');
+    expect(page.getTitleText()).toEqual('bam-app app is running!');
   });
 
   afterEach(async () => {
